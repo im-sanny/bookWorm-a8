2e2bc702d13ae48316175c0197daab150acc9e81
im-sanny <raselparvezsanny@gmail.com>
2024-03-26 00:48:32 +0600

    project setup done

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,182 @@
+const Banner = () => {
+  return (
+    <section className="">
+      <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+        <a
+          rel="noopener noreferrer"
+          href="#"
+          className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 bg-[#1313130D]"
+        >
+          <div className="p-6 space-y-2 lg:col-span-5">
+            <h3 className="text-2xl font-semibold sm:text-4xl">
+              Books to freshen up your bookshelf
+            </h3>
+            <button className="btn bg-[#59C6D2]">View The List</button>
+          </div>
+          <img
+            src="https://source.unsplash.com/random/480x360"
+            alt=""
+            className="object-cover w-full h-64 rounded sm:h-96 lg:col-span-7 dark:bg-gray-500"
+          />
+        </a>
+
+        <div className="my-10 text-4xl font-bold flex justify-center">Books</div>
+
+        <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+          <a
+            rel="noopener noreferrer"
+            href="#"
+            className="max-w-sm mx-auto group hover:no-underline focus:no-underline border-2 p-5 rounded-lg"
+          >
+            <img
+              role="presentation"
+              className="object-cover bg-[#1313130D] w-full rounded h-44"
+              src="https://source.unsplash.com/random/480x360?3"
+            />
+            <div className="p-6 space-y-2">
+              <h3 className="text-2xl font-semibold group-hover:underline group-focus:underline">
+                In usu laoreet repudiare legendos
+              </h3>
+              <span className="text-xs dark:text-gray-600">
+                January 23, 2021
+              </span>
+              <p>
+                Mei ex aliquid eleifend forensibus, quo ad dicta apeirian
+                neglegentur, ex has tantas percipit perfecto. At per tempor
+                albucius perfecto, ei probatus consulatu patrioque mea, ei
+                vocent delicata indoctum pri.
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to='listedBooks'>Listed Books</NavLink>
+          </li>
+          <li>
+            <NavLink to='/pagesToRead'>Pages to Read</NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-end">
+        <a
+          href="#_"
+          className="relative rounded px-5 py-2.5 overflow-hidden group bg-[#23BE0A] hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300 mr-2"
+        >
+          <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          <span className="relative">Sign In</span>
+        </a>
+        <a
+          href="#_"
+          className="relative rounded px-5 py-2.5 overflow-hidden group bg-[#59C6D2] hover:bg-gradient-to-r hover:from-green-500 hover:to-green-400 text-white hover:ring-2 hover:ring-offset-2 hover:ring-green-400 transition-all ease-out duration-300"
+        >
+          <span className="absolute right-0 w-8 h-32 -mt-12 transition-all duration-1000 transform translate-x-12 bg-white opacity-10 rotate-12 group-hover:-translate-x-40 ease"></span>
+          <span className="relative">Sign Up</span>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
